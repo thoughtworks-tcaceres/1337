53a53ce703d065aa6075dcdf1fa3942a62a3b990
tylerccaceres <tyler.caceres@gmail.com>
2019-11-04 18:09:58 -0500

    method 2

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,9 +1,14 @@
+// var containsDuplicate = function(nums) {
+//   nums.sort();
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     if (nums[i] === nums[i + 1]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
 var containsDuplicate = function(nums) {
-  nums.sort();
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] === nums[i + 1]) {
-      return true;
-    }
-  }
-  return false;
+  let setNums = new Set([...nums]);
+  return setNums.size === nums.length ? false : true;
 };
